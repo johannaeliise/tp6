99daa0d1041113dec070b59a36017b0290b7a533
johannaeliise <johannaeliise.teder@wsu.edu>
2022-10-09 23:25:20 +0000

    updated with AJAX

--- scripts.js
+++ scripts.js
@@ -1,7 +1,38 @@
-//JavaScript for Tp5
+//JavaScript for Tp6
+
+
+//function to load file from the URL "fromFile" into the object intetified by "whereTo"
+function loadFileInto(fromFile, whereTo) {
+
+	// creating a new XMLHttpRequest object
+	ajax = new XMLHttpRequest();
+
+	// defines the GET/POST method, source, and async value of the AJAX object
+	ajax.open("GET", fromFile, true);
+
+	// provides code to do something in response to the AJAX request
+	ajax.onreadystatechange = function() {
+			if ((this.readyState == 4) && (this.status == 200)) {
+				document.querySelector(whereTo).innerHTML += this.responseText;
+				
+			} else if ((this.readyState == 4) && (this.status != 200)) {
+				console.log("Error: " + this.responseText);
+			}
+		
+	} // end ajax.onreadystatechange function
+
+	// initiate request and wait for response
+	ajax.send();
+
+}
 
 
 window.onload = function() {
+  
+    
+  loadFileInto("ingredients.html", "#ingredients ul");
+  loadFileInto("equipment.html", "#equipment ul");
+  loadFileInto("directions.html", "#directions ol");
 
   document.querySelector("#titleBanner h1").classList.add("tp");
 
@@ -24,5 +55,6 @@ window.onload = function() {
     
   }
     document.querySelector("#recipeColumns #equipment ul").innerHTML += "<li>Cocoa powder mesh shaker</li>";
+
 
 } // end window-onload
